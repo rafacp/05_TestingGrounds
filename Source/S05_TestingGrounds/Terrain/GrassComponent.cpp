80d24efe661b805cea0ed2d14ae2950ec44b7849
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2017-11-23 19:17:21 -0300

    Flyweight Pattern for foliage

---
+++ Source/S05_TestingGrounds/Terrain/GrassComponent.cpp
@@ -0,0 +1,43 @@
+// Copyright RafaCP
+
+#include "GrassComponent.h"
+
+
+// Sets default values for this component's properties
+UGrassComponent::UGrassComponent()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UGrassComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SpawnGrass();
+	
+}
+
+
+// Called every frame
+void UGrassComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+void UGrassComponent::SpawnGrass()
+{
+	for (size_t i = 0; i < SpawnCount; i++)
+	{
+		FVector Location = FMath::RandPointInBox(SpawningExtents);
+		AddInstance(FTransform(Location));
+	}
+}
+
